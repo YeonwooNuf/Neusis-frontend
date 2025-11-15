eb4c798132d6d05f7ec40a95ba9b977915833148
장연우 <jangyw0927@naver.com>
2025-11-15 23:44:23 +0900

    // 페이지 컨테이너
    공통 레이아웃 래퍼
    반응형 패딩 적용 및 최대 너비 제한(1200px)

---
+++ src/components/PageContainer.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from 'react';
+import './PageContainer.css';
+
+interface PageContainerProps {
+  children: ReactNode;
+  className?: string;
+}
+
+const PageContainer = ({ children, className = '' }: PageContainerProps) => {
+  return (
+    <div className={`page-container ${className}`}>
+      {children}
+    </div>
+  );
+};
+
+export default PageContainer;
+
