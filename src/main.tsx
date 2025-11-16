d135ae414b7022d49ea82f83ca9a329a59a1654d
장연우 <jangyw0927@naver.com>
2025-11-16 22:45:40 +0900

    // StrictMode 제거
    useEffect 2번 호출 방지

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,9 @@ import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+        <AuthProvider>
+            <App />
+        </AuthProvider>
     </BrowserRouter>
-  </React.StrictMode>
 );
