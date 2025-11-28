525b78fb812f5cd35990465264b4fd68ca9cf9fd
장연우 <jangyw0927@naver.com>
2025-11-28 23:26:22 +0900

    // App 전체를 ThemeProvider로 감싸기

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
+import { ThemeProvider } from './contexts/ThemeContext';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <BrowserRouter>
-        <AuthProvider>
-            <App />
-        </AuthProvider>
+      <AuthProvider>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </AuthProvider>
     </BrowserRouter>
 );
